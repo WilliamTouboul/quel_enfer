f0f074daad4f5590711ae6d781db5240834638c0
William Touboul <Touboul.william@gmail.com>
2022-09-11 20:12:51 +0200

    oignons

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -27,8 +27,14 @@ const elem_2 = document.querySelector('.section_element_2');
 const elem_3 = document.querySelector('.section_element_3');
 const elem_4 = document.querySelector('.section_element_4');
 
+const target_1 = document.querySelector('#target_elem_1');
+const target_2 = document.querySelector('#target_elem_2');
+const target_3 = document.querySelector('#target_elem_3');
+const target_4 = document.querySelector('#target_elem_4');
+
+
 var elem_1_data = {
-    img: '/projects/emergence/MAIN.jpg',
+    img: 'projects/emergence/MAIN.jpg',
     title: 'EMERGENCE',
     date: '2021',
     category_1: 'Branding',
@@ -36,7 +42,7 @@ var elem_1_data = {
 }
 
 var elem_2_data = {
-    img: '/projects/silkykiss/MAIN.jpg',
+    img: 'projects/silkykiss/MAIN.jpg',
     title: 'SILKY KISS',
     date: '2022',
     category_1: 'Branding',
@@ -44,121 +50,148 @@ var elem_2_data = {
 }
 
 var elem_3_data = {
-    img: '/projects/softlybrutal/MAIN.jpg',
-    title: 'SILKY KISS',
+    img: 'projects/softlybrutal/MAIN.jpg',
+    title: 'SOFTLY BRUTAL',
     date: '2022',
     category_1: 'Web Design',
     category_2: 'UI/UX Design'
 }
 
 var elem_4_data = {
-    img: '/projects/noise/MAIN.jpg',
+    img: 'projects/noise/MAIN.jpg',
     title: 'NOISE',
     date: '2022',
     category_1: 'DEV',
     category_2: 'Web Design'
 }
 
-elem_1.addEventListener('mouseover', function () {
+target_1.addEventListener('mouseover', function () {
     title.innerHTML = elem_1_data.title;
     date.innerHTML = elem_1_data.date;
     cat_1.innerHTML = elem_1_data.category_1;
     cat_2.innerHTML = elem_1_data.category_2;
     proj_img.src = elem_1_data.img;
-
-    gsap.to('#project_img', {
-        height: '80%',
-        duration: .2,
-    })
-
     proj_img.style.filter = "url('#turbulence')";
-
-    gsap.to('#feTurb', {
+    let tl = gsap.timeline();
+    tl.to('#feTurb', {
         delay: .2,
         attr: {
             ['baseFrequency']: '0',
         },
-        duration: .8,
-
-    })
-
+        duration: 1.8,
+        ease: 'power4.out'
+    });
+    tl.to(title, {
+        opacity: 1,
+    }, 1)
+    tl.to(date, {
+        opacity: 1,
+    }, 1)
+    tl.to(cat_1, {
+        opacity: 1,
+    }, 2)
+    tl.to(cat_2, {
+        opacity: 1,
+    }, 2)
     document.querySelector('#feTurb').setAttribute('baseFrequency', '0.2')
 });
 
-elem_2.addEventListener('mouseover', function () {
+target_2.addEventListener('mouseover', function () {
     title.innerHTML = elem_2_data.title
     date.innerHTML = elem_2_data.date;
     cat_1.innerHTML = elem_2_data.category_1
     cat_2.innerHTML = elem_2_data.category_2
     proj_img.src = elem_2_data.img;
-
-    gsap.to('#project_img', {
-        height: '80%',
-        duration: .2,
-    })
-
     proj_img.style.filter = "url('#turbulence')";
-
-    gsap.to('#feTurb', {
+    let tl = gsap.timeline();
+    tl.to('#feTurb', {
         delay: .2,
         attr: {
             ['baseFrequency']: '0',
         },
-        duration: .8,
-
-    })
-
+        duration: 1.8,
+        ease: 'power4.out'
+    });
+    tl.to(title, {
+        opacity: 1,
+    }, 1)
+    tl.to(date, {
+        opacity: 1,
+    }, 1)
+    tl.to(cat_1, {
+        opacity: 1,
+    }, 2)
+    tl.to(cat_2, {
+        opacity: 1,
+    }, 2)
     document.querySelector('#feTurb').setAttribute('baseFrequency', '0.2')
 });
 
-elem_3.addEventListener('mouseover', function () {
+target_3.addEventListener('mouseover', function () {
     title.innerHTML = elem_3_data.title
     date.innerHTML = elem_3_data.date;
     cat_1.innerHTML = elem_3_data.category_1
     cat_2.innerHTML = elem_3_data.category_2
     proj_img.src = elem_3_data.img;
-
-    gsap.to('#project_img', {
-        height: '80%',
-        duration: .2,
-    })
-
     proj_img.style.filter = "url('#turbulence')";
-
-    gsap.to('#feTurb', {
+    let tl = gsap.timeline();
+    tl.to('#feTurb', {
         delay: .2,
         attr: {
             ['baseFrequency']: '0',
         },
-        duration: .8,
-
-    })
-
+        duration: 1.8,
+        ease: 'power4.out'
+    });
+    tl.to(title, {
+        opacity: 1,
+    }, 1)
+    tl.to(date, {
+        opacity: 1,
+    }, 1)
+    tl.to(cat_1, {
+        opacity: 1,
+    }, 2)
+    tl.to(cat_2, {
+        opacity: 1,
+    }, 2)
     document.querySelector('#feTurb').setAttribute('baseFrequency', '0.2')
 });
 
-elem_4.addEventListener('mouseover', function () {
+target_4.addEventListener('mouseover', function () {
     title.innerHTML = elem_4_data.title
     date.innerHTML = elem_4_data.date;
     cat_1.innerHTML = elem_4_data.category_1
     cat_2.innerHTML = elem_4_data.category_2
     proj_img.src = elem_4_data.img;
-
-    gsap.to('#project_img', {
-        height: '80%',
-        duration: .2,
-    })
-
     proj_img.style.filter = "url('#turbulence')";
-
-    gsap.to('#feTurb', {
+    let tl = gsap.timeline();
+    tl.to('#feTurb', {
         delay: .2,
         attr: {
             ['baseFrequency']: '0',
         },
-        duration: .8,
-
-    })
-
+        duration: 1.8,
+        ease: 'power4.out'
+    });
+    tl.to(title, {
+        opacity: 1,
+    }, 1)
+    tl.to(date, {
+        opacity: 1,
+    }, 1)
+    tl.to(cat_1, {
+        opacity: 1,
+    }, 2)
+    tl.to(cat_2, {
+        opacity: 1,
+    }, 2)
     document.querySelector('#feTurb').setAttribute('baseFrequency', '0.2')
 })
+
+
+
+
+/* --------------------------- button down headerx -------------------------- */
+const button_down = document.querySelector('.nav_arrow');
+
